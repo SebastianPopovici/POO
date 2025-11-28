2b4fb08954e86ab03f23c5d5c167c4ad84fac5d7
Katia-MILOUDI <miloudikatia2020@gmail.com>
2025-11-28 17:06:40 +0100

    Clean up comments in Main.java
    Removed unnecessary comments and documentation.

--- src/main/java/fr/ece/labyrinthegame/Main.java
+++ src/main/java/fr/ece/labyrinthegame/Main.java
@@ -1,13 +1,10 @@
 
 package fr.ece.labyrinthegame;
 
-/**
- * POINT D'ENTRÉE DE L'APPLICATION
- * Ce fichier lance la page de connexion
- */
+//lancer la page de connexion
 public class Main {
     public static void main(String[] args) {
-        // Lancer la page de connexion
+      
         Connexion.main(args);
     }
 }
